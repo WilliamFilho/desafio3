3542c91b6e5cea08df35dd6a568e51fbcf1ba6f8
WilliamFilho <williamfilho20@gmail.com>
2023-06-20 20:27:44 -0300

    --> created endpoint findById ...  <--

--- src/main/java/com/wnet/desafio3/controller/ClientController.java
+++ src/main/java/com/wnet/desafio3/controller/ClientController.java
@@ -1,15 +1,31 @@
 package com.wnet.desafio3.controller;
 
+import com.wnet.desafio3.assembler.ClientAssembler;
+import com.wnet.desafio3.dto.ClientDTO;
+import com.wnet.desafio3.service.ClientService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clients")
+@AllArgsConstructor
 public class ClientController {
 
+    //Usando Lombok para injetar as dependências através do construtor
+    private ClientService service;
+    private ClientAssembler assembler;
     @GetMapping
-    public String teste(){
-        return "Desafio 3";
+    public String desafio(){
+        return "Desafio 3 - Prof: Nélio Alves - Aluno: William Filho";
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
 }
--- src/main/java/com/wnet/desafio3/service/ClientService.java
+++ src/main/java/com/wnet/desafio3/service/ClientService.java
@@ -1,8 +1,23 @@
 package com.wnet.desafio3.service;
 
+import com.wnet.desafio3.assembler.ClientAssembler;
+import com.wnet.desafio3.dto.ClientDTO;
+import com.wnet.desafio3.repository.ClientRepository;
+import com.wnet.desafio3.service.exceptions.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@AllArgsConstructor
 public class ClientService {
 
+    //Usando Lombok para injetar as dependências através do construtor
+    private ClientRepository repository;
+    private ClientAssembler assembler;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        return repository.findById(id).map(product -> assembler.toModel(product)).orElseThrow(()-> new ResourceNotFoundException("Recurso não encontrado!"));
+    }
 }
