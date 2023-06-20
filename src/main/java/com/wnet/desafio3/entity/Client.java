caf509c89a76ba2997de8835cba5341494286f9c
WilliamFilho <williamfilho20@gmail.com>
2023-06-20 19:15:15 -0300

    --> add packages  <--

--- src/main/java/com/wnet/desafio3/entity/Client.java
+++ src/main/java/com/wnet/desafio3/entity/Client.java
@@ -1,8 +1,21 @@
 package com.wnet.desafio3.entity;
 
-import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import java.time.LocalDate;
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String name;
     private String cpf;
