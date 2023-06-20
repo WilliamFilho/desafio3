27fb5429fc7cf84d8f72c2d5d37e8a2fa6aa35cb
WilliamFilho <williamfilho20@gmail.com>
2023-06-20 20:58:08 -0300

    --> created Seeding de dados ...  <--

---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('William Filho', '50908871600', 1000.00, '1990-01-01', 0);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Virgulino Ferreira', '12186827018', 1200.00, '1991-02-07', 4);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('José Serafin', '06145736372', 2500.00, '1985-04-01', 2);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Cilabina Gonçalves', '87233172307', 800.00, '1995-10-25', 1);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Bertilaine Reis', '43292188105', 3000.00, '1998-11-17', 0);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Credilina Morais', '62455739635', 2040.00, '1992-10-01', 3);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Janice Bispo de Roma', '62140591178', 4500.00, '2000-05-15', 4);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Maria da Boa Morte', '35615777508', 7524.00, '1984-09-20', 5);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Edson Lobao', '88828555629', 8500.00, '1997-01-05', 1);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Edna Silva da Costa', '37077546420', 4520.00, '2001-01-30', 2);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Márcio Pereira Abreu', '11337419249', 2540.00, '2002-08-29', 1);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Rômero Silva Sousa', '36758546713', 7500.00, '2003-07-01', 2);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Ana Maria Machado', '24807252941', 5600.00, '2005-01-11', 0);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Amélia Martins Silva', '21848823215', 3600.00, '2006-01-01', 0);
+insert into CLIENT (NAME, CPF, INCOME, BIRTH_DATE, CHILDREN) values ('Tiana Ferreira Gomes', '95680388928', 4875.00, '1996-10-05', 3);
