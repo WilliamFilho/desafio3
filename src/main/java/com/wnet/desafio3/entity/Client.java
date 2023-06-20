61e043b01c82fb88d01a777f42c8d926472b12c1
WilliamFilho <williamfilho20@gmail.com>
2023-06-20 19:10:32 -0300

    --> add perfil test + entity client <--

---
+++ src/main/java/com/wnet/desafio3/entity/Client.java
@@ -0,0 +1,12 @@
+package com.wnet.desafio3.entity;
+
+import java.time.LocalDate;
+
+public class Client {
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+}
